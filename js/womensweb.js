b526c02c275b5a90f13da119eae1140ed2034b0c
evans1mv <122380118+evans1mv@users.noreply.github.com>
2023-04-04 21:31:32 -0400

    Add files via upload

---
+++ js/womensweb.js
@@ -0,0 +1,3 @@
+document.querySelector("#btn").addEventListener("click", function() {
+    alert("Kelsey's DISCLAIMER: I did not know everything!! I'm only human - I'm still growing and learning and I have a limited knowledge of things, but I will do my best and work to make this page as inclusive and unbiased as possible.");
+});
